3ccd3665dc853495864c3a376e6e7b35ca1f4573
Gurjinder Massan <gurjinder.massan@gmail.com>
2019-04-23 22:07:33 +0100

    display comments

---
+++ views/comments/displayOne.php
@@ -0,0 +1,16 @@
+<div class="comment-display" style="display: flex; flex-flow: row wrap;">
+    <div class="avatar" >
+        <img id="avatar" alt="avatar" height="70" width="70" src="../images/Sasha.jpeg">
+    </div>
+    <div class="comment-content" style="padding-left: 10px;">
+        <header class="comment-header">
+        <a href="">Sasha Massan</a>
+        </header>
+        <div class="comment-body" style="width:100%">
+            Great post!
+        </div>
+        <footer class='comment-footer'>
+            <a href="" action="reply">Reply</a>
+        </footer>
+    </div>
+</div>
