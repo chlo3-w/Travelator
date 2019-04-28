942efe3c9b7226f74df90b47f3cebb9447c5a158
gurjindermassan <gurjinder.massan@gmail.com>
2019-04-28 23:22:21 +0100

    display comments (#5)
    * display comments
    
    * updated comments table
    
    * WIP comments
    
    * display comments working and updates to database

---
+++ controllers/comments_controller.php
@@ -0,0 +1,26 @@
+<?php
+
+class CommentsController {
+
+    public function read($id) {
+//        if (!isset($_GET['id']))
+//        {
+//            return call('pages', 'error');
+//        } else {
+//
+//            try {
+//                $comment = Comment::displayComments($_GET['id']);
+//                require_once '../views/comments/displayOne.php';
+//            } catch (Exception $ex) {
+//                return call('pages', 'error');
+//            }
+//        }
+//    }
+       
+        $allComments = Comment::readComments($id);
+        foreach ($allComments as $comment) {
+            require('views/comments/displayOne.php');
+        }
+    }
+
+}
