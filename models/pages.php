2feea3c306f94493b86a19ffbf265dca896e1101
piesMcpies <sheila.kerry10@gmail.com>
2019-04-29 19:53:10 +0100

    changes due to db update

--- models/pages.php
+++ models/pages.php
@@ -5,11 +5,11 @@ class Pages {
         public static function readHomeData() {
         $list = [];
         $db = Db::getInstance();
-        $req = $db->query('call selectHomeData');
+        $req = $db->query('call selectHomeData()');
         // we create a list of Product objects from the database results
          require_once('models/post.php');
         foreach ($req->fetchAll() as $post) {
-            $list[] = new Post($post['id'], $post['title'], $post['content'], $post['img']);
+            $list[] = new Post($post['id'], $post['title'], $post['content'], $post['city']);
         }
         return $list;
     }
