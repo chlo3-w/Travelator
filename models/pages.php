f26ee203e6b4ed8687f648371da9bd3fc95a8aea
cherylhorrigan <46632284+cherylhorrigan@users.noreply.github.com>
2019-04-29 20:12:00 +0100

    Search function WIP (#10)

--- models/pages.php
+++ models/pages.php
@@ -7,7 +7,7 @@ class Pages {
         $db = Db::getInstance();
         $req = $db->query('call selectHomeData');
         // we create a list of Product objects from the database results
-         require_once('models/post.php');
+         require_once('models/post_model.php');
         foreach ($req->fetchAll() as $post) {
             $list[] = new Post($post['id'], $post['title'], $post['content'], $post['img']);
         }
--- views/js/main.js
+++ views/js/main.js
@@ -1,13 +1,13 @@
-
-
 $("path, circle").hover(function(e) {
-  $('#info-box').css('display','block');
-  $('#info-box').html($(this.title));
+  $("#info-box").css("display", "block");
+  $("#info-box").html($(this.title));
 });
 $("path, circle").mouseleave(function(e) {
-  $('#info-box').css('display','none');
+  $("#info-box").css("display", "none");
 });
-$(document).mousemove(function(e) {
-  $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
-  $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
-}).mouseover();
+$(document)
+  .mousemove(function(e) {
+    $("#info-box").css("top", e.pageY - $("#info-box").height() - 30);
+    $("#info-box").css("left", e.pageX - $("#info-box").width() / 2);
+  })
+  .mouseover();
