7d88b44465b940744fe81da7ce3b397856979f8f
Gurjinder Massan <gurjinder.massan@gmail.com>
2019-05-01 22:54:22 +0100

    search with axios

--- api/search.php
+++ api/search.php
@@ -1,22 +1,7 @@
 <?php
-// require_once('models/search_model.php');
+require_once('../models/search_model.php');
 
-// $searchModel = new Search();
+$results = Search::searchResults($_GET['query']);
+print json_encode($results);
 
-// $results = $searchModel::searchResults($_GET['term']);
-// $rows = array();
-// while($r = mysqli_fetch_assoc($results)) {
-//     $rows[] = $r;
-// }
-// print json_encode($rows);
 
-print '[
-    {
-        "id": "1",
-        "title": "Post 1"
-    },
-    {
-        "id": "2",
-        "title": "Post 2"
-    }
-]';
--- controllers/comments_controller.php
+++ controllers/comments_controller.php
@@ -1,4 +1,5 @@
 <?php
+require_once('models/comments_model.php');
 
 class CommentsController {
 
