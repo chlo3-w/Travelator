2c0acfa29acd56551e86048cb8082946d5e8f550
piesMcpies <sheila.kerry10@gmail.com>
2019-05-05 18:30:39 +0100

    merge fixes

--- views/js/main.js
+++ views/js/main.js
@@ -10,25 +10,15 @@
 //    $(this).parent().addClass("focus");
 //  });
 
-$("path, circle").hover(function(e) {
+
+
+
+ $("path, circle").hover(function(e) {
   $('#info-box').css('display','block');
   $('#info-box').html($(this).data('info'));
 });
-
 
 $("path, circle").mouseleave(function(e) {
   $('#info-box').css('display','none');
 });
 
-
-$(document).mousemove(function(e) {
-  $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
-  $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
-}).mouseover();
-
-//$(document).onclick(function(e){
-//    var country = html($(this).data('info'));
-// xmlhttp.open("GET", "?controller=post&action=read&location=" + country, true);
-//
-//    });
-//   
