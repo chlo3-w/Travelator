96c70ede2063d9cc920c51438b95691d7171924b
Gurjinder Massan <gurjinder.massan@gmail.com>
2019-05-06 18:25:43 +0100

    fixed an error to allow submission of comments

--- controllers/comments_controller.php
+++ controllers/comments_controller.php
@@ -6,9 +6,9 @@ class CommentsController {
 
     public function display($id) {
 
-        if (!empty($_POST) && !empty($_SESSION['user_id'])) {
+        if (!empty($_POST) && !empty($_SESSION['userId'])) {
             $parentId = NULL;
-            $userId = ($_SESSION['user_id']);
+            $userId = ($_SESSION['userId']);
 
             if (isset($_POST['parentId'])) {
                 $parentId = filter_input(INPUT_POST, 'parentId', FILTER_SANITIZE_SPECIAL_CHARS);
